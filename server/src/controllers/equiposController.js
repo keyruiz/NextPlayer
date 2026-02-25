a3e7c55879f64ffc5b5b3ea031c11173faba2778
keynis <keydejun@gmail.com>
2026-02-25 19:03:21 +0100

    getEquiposId hecho a falta de comprobar

--- server/src/controllers/equiposController.js
+++ server/src/controllers/equiposController.js
@@ -16,3 +16,20 @@ exports.getTeams = async (req, res) => {
         console.error("Error al obtener los datos:", error.message)
     }
 }
+
+exports.getTeamsId = async (req, res) => {
+    try {
+        const {id } = req.params;
+
+        let query = "SELECT e.*, g.name AS name_game FROM teams e JOIN games g ON e.game_id = g.id WHERE e.id = $1"
+        let response = await pool.query(query, [id])
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: "Equipo no encontrado" });
+        }
+        res.json(result.rows[0])
+    } catch(error) {
+        console.error("Error al obtener el equipo por id", error.message)
+    }
+}
+
+

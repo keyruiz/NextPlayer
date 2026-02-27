df36682ac63c3ace6d4d9ac9ce2369051b259708
keynis <keydejun@gmail.com>
2026-02-27 11:07:53 +0100

    Export de el route de equipos hecho

--- server/src/routers/equiposRoutes.js
+++ server/src/routers/equiposRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const equipoController = require('../controllers/equiposController');
-const router = require('./authRoutes');
-
+const router = express.Router()
 router.get('/', equipoController.getTeams)
 router.get('/:id', equipoController.getTeamsId)
 router.post(`/`, equipoController.postTeams)
 router.put('/:id', equipoController.putTeam)
 router.delete('/:id',equipoController.deleteTeam)
 
+module.exports = router

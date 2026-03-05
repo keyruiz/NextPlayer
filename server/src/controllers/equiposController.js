eb8de2763cdf0f56f6c73ed54b9f6f296562cb0a
keynis <keydejun@gmail.com>
2026-03-05 14:00:00 +0100

    res.status añadidos con el codigo correspondiente en el equiposController

--- server/src/controllers/equiposController.js
+++ server/src/controllers/equiposController.js
@@ -11,24 +11,26 @@ exports.getTeams = async (req, res) => {
         }
 
         const response = await pool.query(queryText, values)
-        res.json(response.rows)
+        res.status(200).json(response.rows)
     } catch(error) {
-        console.error("Error al obtener los datos:", error.message)
+        console.error(error)
+        console.status(500).json({error: 'Error en el servidor al obtener los equipos'})
     }
 }
 
 exports.getTeamsId = async (req, res) => {
     try {
-        const {id } = req.params;
+        const { id } = req.params;
 
         let query = "SELECT e.*, g.name AS name_game FROM teams e JOIN games g ON e.game_id = g.id WHERE e.id = $1"
         let response = await pool.query(query, [id])
-        if (response.rows.length === 0) {
+        if (response.rowCount === 0) {
             return res.status(404).json({ error: "Equipo no encontrado" });
         }
-        res.json(response.rows[0])
+        res.status(200).json(response.rows[0])
     } catch(error) {
-        console.error("Error al obtener el equipo por id", error.message)
+        console.error(error)
+        res.status(500).json({error: 'Error al obtener equipos por id'})
     }
 }
 
@@ -36,18 +38,21 @@ exports.postTeams = async (req, res) => {
     try {
         const {name, game_id, logo, description} = req.body
         const des = description === "" ? null : description
+        //Falta añadir transaccion para hacer que el user cambia a ceo al hacer el equipo
         const nuevoEquipo = await pool.query('INSERT INTO teams (game_id, name, logo, description) VALUES $1, $2, $3, $4 RETURNING id', [game_id, name, logo, des])
-        res.json(nuevoEquipo)
+        res.status(201).json(nuevoEquipo)
     } catch (error) {
-        console.error("Error al insertar el equipo nuevo", error.message)
+        console.error(error)
+        res.status(500).json({error: `Error al crear equipo `})
     }
 }
 
 exports.putTeam = async (req, res) => {
     try {
-        const {id} = req.params
-        const {name, game_id, logo, description} = req.body
+        const { id } = req.params
+        const { name, logo, description } = req.body
         const des = description === "" ? null : description
+        //A falta de añadir validacion si el ceo es el que hace el edit
         const equipoAct = await pool.query(
             `UPDATE teams 
             SET 
@@ -55,19 +60,23 @@ exports.putTeam = async (req, res) => {
                 logo = COALESCE($2, logo),
                 description = COALESCE($3, description)
             WHERE id = $4 
-            RETURNING *`, [name, logo, description, id])
-        res.json(equipoAct)
+            RETURNING *`, [name, logo, des, id])
+        res.status(200).json(equipoAct)
     } catch(error) {
         console.error(error)
+        res.status(500).json({error: 'Error del servidor al actualizar equipo'})
     }
 }
 
 exports.deleteTeam = async (req,res) => {
     try {
-        const {id} = req.params
+        const {id} = req.params7
+        //A falta de añadir validacion si es el CEO
         await pool.query('DELETE FROM teams WHERE id = $1', [id])
+        res.status(200).json({message: 'Equipo borrado con exito'})
     } catch(error) {
         console.error(error)
+        res.status(500).json({error: 'Error del servidor al borrar equipo'})
     }
 }
 
